88e44f57ee0cde756b5762be80d76724a6a14126
paciradukunda <paifique57@gmail.com>
2023-04-11 17:47:54 +0200

    reversing changes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -30,7 +30,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		new_tree_node->left = parent->left;
-		parent->left->parent = new_tree_node;
+		new_tree_node->left->parent = new_tree_node;
 		parent->left = new_tree_node;
 	}
 	return (new_tree_node);
